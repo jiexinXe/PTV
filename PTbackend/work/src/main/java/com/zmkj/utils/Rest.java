776601ed2d879d51463b891a0f3eb53005f1cacd
yuheng <21301016@bjtu.edu.cn>
2024-03-07 19:33:18 +0800

    创建前后端代码

---
+++ PTbackend/work/src/main/java/com/zmkj/utils/Rest.java
@@ -0,0 +1,21 @@
+package com.zmkj.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Rest {
+    private Integer Code;
+    private Object data;
+    private String msg;
+
+    public Rest(Integer code,String msg){
+        this.Code = code;
+        this.msg = msg;
+    }
+
+}
