e6a1464d6bddb9b4565f5bc6d90105ade5b743fc
jiexinXe <jiexinXe@163.com>
2024-04-28 21:39:58 +0800

    Update CargoController.java

--- src/main/java/com/example/ptv/controller/CargoController.java
+++ src/main/java/com/example/ptv/controller/CargoController.java
@@ -8,6 +8,9 @@ import com.example.ptv.utils.Rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 本Controller类用于处理与货物管理相关的请求
  *
@@ -32,5 +35,42 @@ public class CargoController {
         }
     }
 
+    /**
+     * 更新货物信息
+     * 需要上流将cargo对象的数据以body的方式传过来
+     * */
+    @PostMapping("/update")
+    public Rest update(@RequestBody Cargo cargo){
+        System.out.println(cargo);
+        boolean success = cargoService.updateCargo(cargo);
+        if(success)
+            return new Rest(Code.rc200.getCode(), "更新成功");
+        return new Rest(Code.rc400.getCode(), "更新失败");
+    }
+    /**
+     * 提取货物的接口
+     * */
+    @DeleteMapping("/delete")
+    public Rest deleteCargo(@RequestParam("id") String cid, @RequestParam("num")String num){
+        System.out.println(cid);
+        return cargoService.deleteCargo(cid,num);
+    }
+
+    /**
+     * 获取某一个用户的所有货物
+     * */
+    @GetMapping("/list/userid")
+    public Rest getCargoListByUserId(@RequestParam("userid")String id){
+        return cargoService.getCargoListByUserId(id);
+    }
+
+    @GetMapping("/one")
+    public Rest getOneCargo(@RequestParam("cid")String cid){
+
+        Cargo cargo = cargoService.getCargoById(cid);
+        Map<Object, Object> ans = new HashMap<>();
+        ans.put("cargo", cargo);
+        return new Rest(Code.rc200.getCode(), ans, "货物信息");
+    }
 
 }
