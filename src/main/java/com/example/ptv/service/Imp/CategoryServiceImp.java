3a403fc529a1fabc26ba5541a13171af0496634f
CecilRiver <2910347807@qq.com>
2024-04-29 16:22:16 +0800

    增加获取所有存在类别的接口

--- src/main/java/com/example/ptv/service/Imp/CategoryServiceImp.java
+++ src/main/java/com/example/ptv/service/Imp/CategoryServiceImp.java
@@ -1,8 +1,22 @@
 package com.example.ptv.service.Imp;
 
+import com.example.ptv.dao.CategoryDao;
+import com.example.ptv.entity.Category;
 import com.example.ptv.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryServiceImp implements CategoryService {
+    @Autowired
+    private CategoryDao categoryDao;
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryDao.selectList(null);
+    }
+
+
 }
